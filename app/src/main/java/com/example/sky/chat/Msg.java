bf6392ba742ea79112a02b3e4134aac633a47e88
skysunlei <4868569@qq.com>
2017-02-24 14:51:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sky/chat/Msg.java
@@ -0,0 +1,30 @@
+package com.example.sky.chat;
+
+/**
+ * Created by sky on 2017/2/24.
+ */
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+    private String content;
+    private int type;
+
+    /**
+     * content表示消息的内容，type表示消息的类型。其中消息类型有两个值可选，TYPE_RECEIVED表示这是一条收到的消息，TYPE_SENT表示这是一条发出的消息
+     * @param content
+     * @param type
+     */
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
